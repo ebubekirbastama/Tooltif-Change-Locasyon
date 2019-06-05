984bd1595e9349e5dc0ccad550865a2610913bc0
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-06-05 08:07:52 +0300

    C# Tooltip Change Locasyon
    C# Tooltip Change Locasyon

---
+++ TooltifChangeLocasyon/Mytooltip.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TooltifChangeLocasyon
+{
+    class Mytooltip : ToolTip
+    {
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        static extern bool MoveWindow(IntPtr h, int x, int y, int width, int height, bool redraw);
+        public Mytooltip()
+        {
+            this.OwnerDraw = true;
+            this.Draw += Benimtooltip_Draw;
+        }
+
+        private void Benimtooltip_Draw(object sender, DrawToolTipEventArgs e)
+        {
+            e.DrawBackground();
+            e.DrawBorder();
+            e.DrawText();
+            var t = (ToolTip)sender;
+            var h = t.GetType().GetProperty("Handle",
+              System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var handle = (IntPtr)h.GetValue(t);
+            var location = new Point(650, 650);
+            var ss = MoveWindow(handle, location.X, location.Y, e.Bounds.Width, e.Bounds.Height, false);
+        }
+    }
+}
